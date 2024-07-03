c9d897fe4f080520596998336423d675d3cdf5c6
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2024-07-03 16:26:08 +0000

    updated files

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,11 @@
-#executes a command
+# executes a command
+
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
