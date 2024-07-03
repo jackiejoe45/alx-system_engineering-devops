ad39f02a74744722246d2eba6f6896cf9389f679
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2024-07-03 00:23:19 +0000

    Completed Tasks

--- 0x05-processes_and_signals/0-what-is-my-pid.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# prints the process ID of the current bash process
-echo $$ 
-
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,12 @@
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
+
+exec { 'pkill_flask':
+  command => 'pkill -f flask',
+  onlyif  => 'pgrep -f flask',
+  path    => ['/usr/bin', '/usr/sbin'],
+  require => Exec['install_flask'],
+}
